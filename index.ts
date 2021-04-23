dc86fd5e8a9058e1d4797e520f609b52724f7f60
cakeinpanic <cakeinpanic@gmail.com>
2021-04-23 19:31:35 +0300

    third

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import stripAnsi from 'strip-ansi'
-import { sendToSlack } from './slack'
+import { sendToRandomFixer } from './slack'
 
 function extractFailedTestsMessages(filePath = './jest.json'): string[] {
   const metadata = JSON.parse(fs.readFileSync(filePath).toString())
@@ -8,4 +8,4 @@ function extractFailedTestsMessages(filePath = './jest.json'): string[] {
   return failedTests.map(({ message }) => stripAnsi(message))
 }
 
-sendToSlack(extractFailedTestsMessages().join(''))
+extractFailedTestsMessages().forEach(sendToRandomFixer)
--- slack.ts
+++ slack.ts
@@ -1,10 +1,12 @@
+import { getInput } from '@actions/core'
 import { ChatPostMessageArguments, WebClient } from '@slack/web-api'
 
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-const token = process.env.SLACK_TOKEN
-const channelId = process.env.CHANNEL_ID
+const token = process.env.SLACK_TOKEN || getInput('slack-token', { required: true })
+const channelId = process.env.CHANNEL_ID || getInput('slack-channel', { required: true })
 const web = new WebClient(token)
 
 export const sendToSlack = async (phrase) => {
@@ -21,4 +23,13 @@ export const sendToSlack = async (phrase) => {
     ]
   } as ChatPostMessageArguments)
   console.log('Message sent: ', res.ts)
+}
+
+
+import randomItem from 'random-item'
+const fixersList: string[] = (process.env.FIXERS || getInput('fixers', { required: true })).split(',')
+
+export const sendToRandomFixer = (message: string) => {
+  const fixer = randomItem(fixersList)
+  sendToSlack(`Lucky one is <@${fixer}>! You are honoured to fix this one:\n \`\`\`${message}\`\`\``)
 }

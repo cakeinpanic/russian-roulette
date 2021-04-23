80a0492edd5cc4a8fb900b87f978b23ac9b886d5
cakeinpanic <cakeinpanic@gmail.com>
2021-04-23 19:21:45 +0300

    first

---
+++ index.ts
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import stripAnsi from 'strip-ansi'
+
+function extractFailedTestsMessages(filePath = './jest.json'): string[] {
+  const metadata = JSON.parse(fs.readFileSync(filePath).toString())
+  const failedTests = metadata.testResults.filter(({ status }) => status === 'failed')
+  return failedTests.map(({ message }) => stripAnsi(message))
+}
+
+console.log(extractFailedTestsMessages())
